d53131c5e056da4326d224e1b06fd7010fd61263
Alex <alexandertaylor315@gmail.com>
2023-03-27 23:59:10 -0400

    added insert_node, search_node, and inorder_traversal functions to binary_node.py file in order to add node to tree, search for a node in the tree, and to perform an in order traversal to nodes in a tree

--- binary_node.py
+++ binary_node.py
@@ -1,4 +1,46 @@
 
 
 class Binary_Node:
-    
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.val = data
+
+def insert_node(self, data):
+    if self is None:
+        return Binary_Node(data)
+    else:
+        if self.val == data:
+            return self
+        elif self.val < data:
+            self.right = insert_node(self.right, data)
+        else:
+            self.left = insert_node(self.left, data)
+    return self
+
+def search_node(self, data):
+    if self is None or self.val == data:
+        return '{} has been found'.format(self.val)
+    elif self.val < data:
+        return search_node(self.right, data)
+    else:
+        return search_node(self.left, data)
+ 
+
+def inorder_traversal(self):
+    if self:
+        inorder_traversal(self.left)
+        print(self.val, end =" ")
+        inorder_traversal(self.right)
+
+if __name__ == '__main__':
+
+    tree = Binary_Node(50)
+    tree = insert_node(tree, 30)
+    tree = insert_node(tree, 20)
+    tree = insert_node(tree, 40)
+    tree = insert_node(tree, 70)
+    tree = insert_node(tree, 60)
+    tree = insert_node(tree, 80)
+    inorder_traversal(tree)
+    print(search_node(tree, 40))
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import implementation
 from linkedlist import LinkedList
+from binary_node import Binary_Node
+from binary_node import insert_node
+from binary_node import inorder_traversal
+from binary_node import search_node
 
 if __name__ == '__main__':
-    linked_list = LinkedList()
+    # linked_list = LinkedList()
+
     
-linked_list.append_node(22)
-linked_list.append_node(33)
-linked_list.append_node(44)
-print(linked_list.find_node(33))
+# linked_list.append_node(22)
+# linked_list.append_node(33)
+# linked_list.append_node(44)
+# print(linked_list.find_node(33))
+
+
+    tree = Binary_Node(50)
+    tree = insert_node(tree, 30)
+    tree = insert_node(tree, 20)
+    tree = insert_node(tree, 40)
+    tree = insert_node(tree, 70)
+    tree = insert_node(tree, 60)
+    tree = insert_node(tree, 80)
+
+    inorder_traversal(tree)
+    print(search_node(tree, 40))
